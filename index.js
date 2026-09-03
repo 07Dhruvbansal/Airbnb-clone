b3ab1074976065efcdbe914022a7b3b2efcebf22
Dhruv Bansal <bansaldhruv121@gmail.com>
2026-09-03 14:23:49 +0530

    Changes occur

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 
